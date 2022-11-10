aa4b8274447d755391159427b1d2e61ff891ffb5
Luis Ponce <luisferpo13@gmail.com>
2022-11-09 22:53:40 -0600

    add: queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users').then((res) => res.json()).then((res) => setData(res.data))
+  }, [])
+
+  const getSecondPage = () => {
+    fetch('https://reqres.in/api/users?page=2').then((res) => res.json()).then((res) => setData(res.data))
+  }
+
+  const getThirdPage = () => {
+    fetch('https://reqres.in/api/users?page=3').then((res) => res.json()).then((res) => setData(res.data))
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button onClick={getSecondPage}>Página 2</button>
+    <button onClick={getThirdPage}>Página 3</button>
+      {
+        data.map((user) => (
+          <div key={user.id}>
+            <p>{`${user.first_name} ${user.last_name}`}</p>
+            <p>{user.email}</p>
+            <img src={user.avatar} />
+            <br />
+          </div>
+        ))
+      }
     </div>
   )
 }
