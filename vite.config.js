754aee74b75bcc2130c451f632f5d8cf510b102d
Luis Ponce <luisferpo13@gmail.com>
2022-11-10 16:10:56 -0600

    add: netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
-import manifest from './manifest.json';
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactSvgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    reactRefresh(),
+    reactSvgrPlugin(),
     VitePWA({
       strategies: 'generateSW',
       registerType: 'autoUpdate',
@@ -15,7 +18,24 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest,
+      manifest: {
+        "name": "Funcapital",
+        "short_name": "Fun",
+        "description": "My Awesome Funcapital app",
+        "theme_color": "#ffffff",
+        "icons": [
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      },
       devOptions: {
         enabled: true,
       },
